9ba3a66e1a582f3e0002f4fa04561884a4004663
Ashwin  Mohan <mohanashwin999@gmail.com>
2020-06-13 19:30:31 +0530

    app routing done

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,15 @@
+import { HomeComponent } from './../home/home.component';
+import { ContactComponent } from './../contact/contact.component';
+import { AboutComponent } from './../about/about.component';
+import { MenuComponent } from './../menu/menu.component';
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,21 +9,29 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
